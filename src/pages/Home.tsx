11e433efe4fffbf15b3af91121d9dc49a825e003
Kauã Neves <73667960+Elkaka-neti@users.noreply.github.com>
2026-01-05 22:09:50 +0000

    feat: adicionar componente Home com layout e integração do TopicInput

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+
+import TopicInput from '@/components/TopicInput';
+
+export function Home() {
+
+    return (
+<div className="flex flex-col items-center justify-center ">
+  <div className="flex-col items-center text-center p-5">
+   <h1 className="text-lg bg-blue-300 rounded-md px-2 ph-2 font-bold text-white"> Teste seus conhecimentos </h1>
+   <p> xxxx </p>
+  </div>
+  
+    <TopicInput />
+  
+</div>
+    )
+}
